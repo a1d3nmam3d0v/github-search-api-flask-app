7a6e5e4969a7d84f82684ff3c87189befe256c72
a1d3n <$>
2022-10-08 23:45:43 -0500

    First commit

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template, request  # NOT the same as requests 
+from flask import Flask, render_template, request  # NOT the same as requests
+from github_api import get_github_user
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")  # homepage
 def homepage():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
+@app.route("/get_user")
+def get_user_info():
+    # get user info from github api, display on page
+    print("form data is", request.args)
+    username = request.args.get("username")  # safer - returns None if no username
+    # username = requests.args["username"]  # more common but errors if no username
+    user_info, error_message = get_github_user(username)  # user_info is a  dictionary
+    if error_message:
+        return render_template("error.html", error=error_message)
+    else:
+        return render_template("github.html", user_info=user_info)
+
+
+if __name__ == "__main__":
     app.run()
--- github_api.py
+++ github_api.py
@@ -1,1 +1,36 @@
-# TODO make requests to github API
+#  make requests to github API
+
+import requests
+import logging
+
+# 404 username not found
+# other errors
+# success
+
+
+def get_github_user(username):
+    #   return a tuple of (data, error)
+    #   if things work return (data, None)
+    #   if they don't work, return (None, error)
+    try:
+        response = requests.get(f"https://api.github.com/users/{username}")
+        if response.status_code == 404:  # not found
+            return None, f"Username {username} not found"
+        response.raise_for_status()
+        response_json = response.json()
+        user_info = extract_user_info(response_json)
+        return user_info, None  # return data, error
+    except Exception as e:
+        logging.exception(e)
+        return None, "Error connecting to Github"
+
+
+def extract_user_info(json_response):
+    return {
+        "login": json_response.get("login"),
+        "name": json_response.get("name"),
+        "avatar_url": json_response.get("avatar_url"),
+        "home_page": json_response.get("html_url"),
+        "login": json_response.get("login"),
+        "repos": json_response.get("public_repos"),
+    }
